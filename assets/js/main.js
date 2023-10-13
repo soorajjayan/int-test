47d0f04db16bb4452366327e444eb20a4b53a044
Sooraj KJ <soorajkj194@gmail.com>
2023-10-14 03:53:06 +0530

    create filter section

--- assets/js/main.js
+++ assets/js/main.js
@@ -291,3 +291,20 @@
    */
   new PureCounter();
 })();
+var portfolioIsotope = $(".portfolio-container").isotope({
+  itemSelector: ".portfolio-item",
+  layoutMode: "fitRows",
+});
+$("#portfolio-flters li").on("click", function () {
+  $("#portfolio-flters li").removeClass("active");
+  $(this).addClass("active");
+
+  portfolioIsotope.isotope({ filter: $(this).data("filter") });
+});
+
+$("#bologna-list a").on("click", function (e) {
+  e.preventDefault();
+  $(this).tab("show");
+});
+
+// test section
